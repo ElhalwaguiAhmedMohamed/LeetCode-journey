cc7cae5932b88d3de0dc510af39e1e2ab03fce79
Mohamed Ahmed Elhalwagui <73799728+ElhalwaguiAhmedMohamed@users.noreply.github.com>
2022-08-04 18:43:14 +0200

    Time: 186 ms (37.92%), Space: 39.6 MB (94.70%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int PivotIndex(int[] nums) {
+        int leftSum=0;
+            int rightSum=0; 
+            for(int i = 0; i < nums.Length; i++)
+            {
+                rightSum+=nums[i];
+            }
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                rightSum-=nums[i];
+                if(leftSum==rightSum)
+                    return i;
+                leftSum+=nums[i];
+            }
+            return -1;
+    }
+}
